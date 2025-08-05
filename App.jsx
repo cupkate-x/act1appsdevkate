bcec825ee329150cdd0a5a5bb162d8999a16da58
Kate Trajano <katetrajano@gmail.com>
2025-08-06 06:15:01 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import Header from "../src/components/Header";
+import Profile from "../src/components/Profile";
+import Footer from "../src/components/Footer";
+
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Profile />
+      <Footer />
+      
+    </div>
+    
+  );
+}
+
+export default App;
